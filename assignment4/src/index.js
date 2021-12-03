50de81f794797bc16ba2a092df76232aade0a6e7
shipley7 <ashiple4@uwo.ca>
2021-12-03 13:23:14 -0500

    Applying calculateReservation functionality

--- assignment4/src/index.js
+++ assignment4/src/index.js
@@ -7,21 +7,15 @@ const app = express();
 
 const port = 4000;
 
+function newConnection() {
 const db = mysql.createConnection ({
     host: 'localhost', 
     user: 'root',  
     password: 'root', 
     database: 'hospitalAdmin'
 });
-  
-db.connect((err) => {
-    if (err) {
-      throw err; 
-    } 
-    console.log('Connected to database'); 
-});
-
-global.db = db;
+return db;
+}
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -41,6 +35,30 @@ app.get('/SearchSchedules', (req,res) => {
 
 app.get('/CalcReservationCost', (req,res) => {
     res.sendFile('staticContent/calcReservationCost.html', {root: __dirname })
+})
+
+app.post('/calculateCost', (req, res) => {
+    let conn = newConnection();
+    conn.connect();
+    let cost = 0;
+    conn.query(`SELECT (equipmentCost + roomCost + 200) AS total \n` +
+             `FROM room, medicalEquipment \n` +
+             `WHERE roomNo = ${req.get("roomNo")} AND equipmentID = ${req.get("equipmentID")}`
+        ,(err,rows,fields) => {
+            if(err) {
+                console.log(err);
+            }
+            else {
+                for (let r of rows) {
+                    cost += r.total;
+                }
+                if (cost>0)
+                    res.send("Your computed cost for any reservation concerning that room and piece of equipment is $" + cost.toFixed(2));
+                else
+                    res.send("Sorry, but one or more of your inputs were illegal at this time");
+            }
+        });
+    conn.end();
 })
 
 app.get('/InsertPatient', (req,res) => {
